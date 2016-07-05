6dd85eedab8b02a5c06eadca51069a2d460b6072
nikznuk <niklas@bbtech-online.de>
2016-07-05 20:56:13 +0200

    jsonGegner eingefügt

---
+++ Task Monsters 0.01/Assets/Scripts/Animy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animy {
+
+	public string name { get; set;}
+	public int health { get; set;}
+	public int attack_1_Damage { get; set;}
+	public int attack_2_Damage { get; set;}
+	public int attack_3_Damage { get; set;}
+	public int attack_4_Damage { get; set;}
+	public bool attack_1_activated { get; set;}
+	public bool attack_2_activated { get; set;}
+	public bool attack_3_activated { get; set;}
+	public bool attack_4_activated { get; set;}
+}
